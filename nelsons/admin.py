cac71ddba902c58d7a0ab2cbbbc88f9600194b89
Baptiste Gaultier <b.gaultier@gmail.com>
2016-03-20 09:42:56 -0400

    Minor changes : edx_user is not used anymore, DELETE Rest verb is allowed now

--- nelsons/admin.py
+++ nelsons/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Nelson
 
-class NelsonAdmin(admin.ModelAdmin):    
-	list_display = ('name', 'edx_user', 'created_date', 'was_active_recently')
+class NelsonAdmin(admin.ModelAdmin):
+	list_display = ('name', 'created_date', 'was_active_recently')
 
 admin.site.register(Nelson, NelsonAdmin)
--- nelsons/models.py
+++ nelsons/models.py
@@ -8,7 +8,6 @@ from datetime import timedelta
 
 class Nelson(models.Model):
 	name = models.SlugField(_(u"Name"), help_text=_(u"Enter a valid Nelson name consisting of letters, numbers, underscores or hyphens."), primary_key=True, max_length=24)
-	edx_user = models.SlugField(_(u"edX User"), help_text=_(u"Enter a valid edx User ID consisting of letters, numbers, underscores or hyphens."), max_length=32, unique=True)
 
 	latitude = models.DecimalField(_(u"Latitude"), max_digits = 10, decimal_places = 6, help_text=_(u"Enter a valid latitude like 48.120094"), null = True, blank = True, default = 48.120094)
 	longitude = models.DecimalField(_(u"Longitude"), max_digits = 10, decimal_places = 6, help_text=_(u"Enter a valid longitude like -1.629206"), null = True, blank = True, default = -1.629206)
