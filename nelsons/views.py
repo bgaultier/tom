7ddb5e466cdf6733ecacd4e1d6230dc517d54f43
Simon Landrault <simon.landrault@qowisio.com>
2016-04-14 16:08:38 +0200

    Little pythonic cleaning

--- nelsons/views.py
+++ nelsons/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView, CreateView
 
@@ -7,11 +6,13 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from .models import Nelson
 from .serializers import NelsonSerializer
 
+
 class NelsonListView(ListView):
     model = Nelson
     queryset = Nelson.objects.filter()
     context_object_name = 'nelson_list'
     template_name = 'nelsons/list.html'
+
 
 class NelsonCreateView(CreateView):
     model = Nelson
@@ -19,6 +20,7 @@ class NelsonCreateView(CreateView):
     template_name = 'nelsons/create.html'
     success_url = reverse_lazy('list')
 
+
 class NelsonDetailView(RetrieveUpdateDestroyAPIView):
-	queryset = Nelson.objects.all()
-	serializer_class = NelsonSerializer
+    queryset = Nelson.objects.all()
+    serializer_class = NelsonSerializer
