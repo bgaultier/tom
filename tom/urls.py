07415c17bf42e1269d1f3fe6ea32aa6d16c8b124
Baptiste Gaultier <b.gaultier@gmail.com>
2016-05-24 07:45:35 +0200

    default home view added

--- tom/urls.py
+++ tom/urls.py
@@ -6,7 +6,7 @@ from . import views
 from hits import views as hits_views
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(pattern_name='list'), name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^nelsons/', include('nelsons.urls', namespace='nelsons')),
     url(r'^admin/', admin.site.urls),
     url(r'^hits/', hits_views.index),
