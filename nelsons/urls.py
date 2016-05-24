fe01bf32f31e41d6399339879a46cce873e19eb6
Baptiste Gaultier <baptiste@vps265917.ovh.net>
2016-05-24 18:42:18 +0200

    allow nelson URI without a final /

--- nelsons/urls.py
+++ nelsons/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', NelsonListView.as_view(), name='list'),
     url(r'^create/?(?P<name>[-\w\d]*)/$', NelsonCreateView.as_view(), name='create'),
     url(r'^check/$', check_existence, name='check'),
+    url(r'^(?P<pk>[-\w\d]+)$', NelsonDetailView.as_view(), name='api'),
     url(r'^(?P<pk>[-\w\d]+)/$', NelsonDetailView.as_view(), name='api'),
 ]
